1b3d6ae0b454855496b1b391e90bfa9347134ec4
dcoffi <daivinson@gmail.com>
2023-11-23 22:46:23 -0400

    M07

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,44 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
-}
+   class Usuario {
+      constructor(usuario, nombre, email, password) {
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+      saludar() {
+         return `Hola, mi nombre es ${this.nombre}`;
+         }
+   }
+   return Usuario;
+}   
+
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function() {
+    return "Hello World!"
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse=function(){
+      return this.split('').reverse().join('');
+   };
+/*
+En este ejemplo, el método reverse se añade al prototipo de String. 
+Se utiliza la función split('') para convertir la cadena en un array de caracteres, 
+luego utiliza reverse() para invertir el orden de los elementos en el array 
+y finalmente, join('') para volver a convertir el array en una cadena. 
+El uso de this se refiere a la cadena en la que se llama el método.
+*/
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
