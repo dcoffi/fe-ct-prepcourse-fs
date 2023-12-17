341ddd57d66c559c9754aa007528e897c3132bae
dcoffi <daivinson@gmail.com>
2023-12-17 11:35:19 -0400

    M09

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,21 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var claves = Object.keys(objeto);
+  
+  // Crea un nuevo array utilizando map
+  /*
+En este código, Object.keys(objeto) se utiliza para obtener un array de las claves del objeto. 
+Luego, map se utiliza para iterar sobre ese array de claves y crear un nuevo array que contiene 
+subarrays con cada par clave-valor. Finalmente, el array resultante se devuelve.
+  */
+  var arrayDeArrays = claves.map(function(clave) {
+    // Por cada clave, crea un subarray con la clave y el valor correspondiente
+    return [clave, objeto[clave]];
+  });
+  
+  return arrayDeArrays;
+
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +29,33 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var frecuenciaLetras = {};
+
+   // Recorrer el string
+   for (var i = 0; i < string.length; i++) {
+     var letra = string[i];
+ 
+     // Verificar si la letra ya está en el objeto
+     if (frecuenciaLetras[letra]) {
+       // Si está presente, incrementar la frecuencia
+       frecuenciaLetras[letra]++;
+     } else {
+       // Si no está presente, inicializar la frecuencia en 1
+       frecuenciaLetras[letra] = 1;
+     }
+   }
+ 
+   // Ordenar las claves alfabéticamente
+   var letrasOrdenadas = Object.keys(frecuenciaLetras).sort();
+ 
+   // Construir el objeto resultado con las letras ordenadas
+   var resultado = {};
+   for (var i = 0; i < letrasOrdenadas.length; i++) {
+     var letraOrdenada = letrasOrdenadas[i];
+     resultado[letraOrdenada] = frecuenciaLetras[letraOrdenada];
+   }
+ 
+   return resultado;
 }
 
 function capToFront(string) {
@@ -22,25 +64,55 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
-}
+   var mayusculas = '';
+   var minusculas = '';
+   for (var i = 0; i < string.length; i++) {
+      if (string[i].match(/^[A-Z]$/)) {
+         mayusculas += string[i];
+         } else {
+            minusculas += string[i];
+            }
 
+}
+return mayusculas+minusculas;
+}
+capToFront('soyHENRY');
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
-}
+   // Separamos la frase en una matriz de palabras
+   var palabras = frase.split(' ');
+   // Recorremos la matriz para invertir cada palabra
+   for (var i=0 ; i<palabras.length ; i++){
+      palabras[i]=palabras[i].split("").reverse().join("");
+      };
+      // Volvemos a juntar todo en una sola cadena separando por espacios
+      return palabras.join(" ");
+      }
+      // ------------------------------------------------
+    
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   numeroStr = numero.toString();
+   reversaNumero = numeroStr.split("").reverse().join("");
+   if (reversaNumero == numeroStr){
+      return "Es capicua";
+      }else{
+         return "No es capicua";
+      }
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   return string.replace(/[abc]/gi, "");
+   
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +121,12 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+
+   arrayOfStrings.sort((a, b)=>{
+      return a.length-b.length;
+      });
+      
+   return arrayOfStrings;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,7 +136,23 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   let interseccion = [];
+   let vacio=[];
+   let ocurre=false;
+   for (let i=0;i<array1.length;i++){
+      if (array2.includes(array1[i])){
+         ocurre=true;
+         interseccion.push(array1[i]);
+      };
+   };
+   if(ocurre){
+      return interseccion;
+   }else{
+      return vacio;
+   }
+         
 }
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
@@ -70,4 +164,5 @@ module.exports = {
    deleteAbc,
    sortArray,
    buscoInterseccion,
+   
 };
